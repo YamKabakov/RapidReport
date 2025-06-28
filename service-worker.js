eeb9c3c12ab45ce74ac40893b3ba712e48bd7b16
YamKabakov <kabakovyam@gmail.com>
2025-06-28 17:50:24 +0300

    again

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ const cacheFiles = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("v1.0.9").then(cache => {
+    caches.open("v1.0.10").then(cache => {
       return cache.addAll(cacheFiles);
     })
   );
