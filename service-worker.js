5dcb2e5ff96963f5fd29c3551e0a6e10b55ab84c
YamKabakov <kabakovyam@gmail.com>
2025-06-29 14:56:26 +0300

    idk

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+const CACHE_NAME = "v1.0.22";
+
 const cacheFiles = [
   "./",
   "./index.html",
@@ -8,8 +10,18 @@ const cacheFiles = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("v1.0.21").then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
